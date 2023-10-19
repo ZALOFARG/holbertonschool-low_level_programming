e63ddd13dbbb4dbcbb7189cc640983cd314e47b0
zalofarg <andre.work.go@gmail.com>
2023-10-19 15:48:49 -0700

    eliminacion del fallo

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
+/**
+ * void
+ *
+ * no return
+ *
+ * Description: puts the alphabet from a til z
+ */
 void print_alphabet(void)
 {
 	char letra;
-	for(letra = 'a'; letra <= 'z'; letra++)
+
+	for (letra = 'a'; letra <= 'z'; letra++)
 	{
 		_putchar(letra);
 	}
