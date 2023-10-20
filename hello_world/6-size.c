e419f04343599934f1086a9511185b2c0f15fd96
zalofarg <andre.work.go@gmail.com>
2023-10-20 15:28:33 -0700

    commit task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * 
+ * Description:
+ *
+ * Return (0) on success
+ *
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+	return (0);
+}
