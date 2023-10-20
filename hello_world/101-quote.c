c289b26daf2981ac2c0412cce7e4758659571986
zalofarg <andre.work.go@gmail.com>
2023-10-20 16:16:24 -0700

    commit t9

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ *
+ * Description: sends message to stderror
+ *
+ * Return: on success (1)
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
