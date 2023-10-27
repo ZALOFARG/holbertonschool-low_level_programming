83aeca3eebd992762169e9cbe2c8897a755c8b29
ZALOFARG <andre.work.go@gmail.com>
2023-10-27 08:08:16 -0700

    commit task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
 /**
- * _puts - prints a string backwards
+ * print_rev - prints a string backwards
  * @str: pointer that reference the string to be printed
  * Description: prints a string backwards referenced on anotther function
  * Return: nothing on sucess
  */
 void print_rev(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (str[i] = '\0'; i = 0; i--)
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(str[i]);
 	}
