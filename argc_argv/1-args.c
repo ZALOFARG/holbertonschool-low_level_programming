3f429918e0acb86a96710cc942c730241a9de0b1
ZALOFARG <andre.work.go@gmail.com>
2023-11-02 19:30:47 -0700

    commit task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: counter
+ * @argv: array
+ *
+ * Description: returns the number of arguments
+ *
+ * Return: on success (0)
+ */
+int main(int argc,__attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
