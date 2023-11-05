f53778db0fdc264a76c65e732e5c68a4086ca714
ZALOFARG <andre.work.go@gmail.com>
2023-11-05 12:43:58 -0800

    introduced betty doc

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - check the code
+ * @s: string to be evaluated
+ *
+ * Description: counts the number of word of a sentence
+ *
+ * Return: check the code
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
