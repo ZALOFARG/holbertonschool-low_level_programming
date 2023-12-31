fa8475186a3b452445b02a25c7b76ab8a12f1036
ZALOFARG <andre.work.go@gmail.com>
2023-12-31 10:31:41 -0800

    betty comp

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,10 +4,11 @@
  * _isalpha - returns if its lowercase or uppercase
  * @c: integer to be evaluated
  *
- * Desciption: evaluates if an integer is a letter, lowercase or uppercase
+ * Description: evaluates if an integer is a letter, lowercase or uppercase
  *
  * Return: (1) if does; (0) if doesnt
  */
+
 int _isalpha(int c)
 {
 	if (((c >= 65) && (c < 91)) || ((c >= 97) && (c < 123)))
