30d519247b267e65ea2ad5a31931a857a65e519b
ZALOFARG <andre.work.go@gmail.com>
2023-12-30 20:02:44 -0800

    1st commit t2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints a struct dog
+ * @d: Pointer to type struct dog
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
